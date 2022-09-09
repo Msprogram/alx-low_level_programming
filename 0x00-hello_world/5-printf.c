d399706575fa6d26b8a34c55b4dd989e90850056
Msprogram <shamuelmoses@gmail.com>
2022-09-08 18:54:44 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return:1
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+
+}
